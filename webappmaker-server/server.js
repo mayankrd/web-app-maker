bff1cf08d82ca1474fa69f1d24ad8fa6219d3168
Mayank Ranjan Dayal <mayank.dyl@gmail.com>
2016-11-08 11:03:49 -0500

    refactored code

--- webappmaker-server/server.js
+++ webappmaker-server/server.js
@@ -7,10 +7,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/webappmaker-client/public'));
+app.use(express.static(__dirname + '/../webappmaker-client/public'));
 
 require ("./test/app.js")(app);
-require("./webappmaker-server/app.js")(app);
+require("./app.js")(app);
+
+// install, load, and configure body parser module
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
