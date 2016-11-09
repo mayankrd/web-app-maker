8e9a0866cc1b7ce8110755d0afa21e0283928612
Mayank Ranjan Dayal <mayank.dyl@gmail.com>
2016-11-09 01:24:34 -0500

    refactored code

--- webappmaker-server/test/app.js
+++ webappmaker-server/test/app.js
@@ -1,22 +1,23 @@
+/**
+ * Created by: Mayank
+ * Client service for User views
+ * Registered app by Angular module name WebAppMaker
+ * Using REST architecture
+ */
+
 module.exports = function(app)
 {
     app.get("/api/test", findAllMessages);
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
+    // local machine connection string for MongoDB
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    //'mongodb://username:password@ds033076.mlab.com:33076/webdev-mayank';
-
-    if(process.env.MLAB_USERNAME) {
-        /*connectionString = "mongodb://"+ process.env.MLAB_USERNAME + ":" +
-         process.env.MLAB_PASSWORD + "@" +
-         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-         process.env.OPENSHIFT_APP_NAME;*/
-
-
-        if(process.env.MLAB_USERNAME) {
+    if(process.env.MLAB_USERNAME)
+    {
+        if(process.env.MLAB_USERNAME)
+        {
             connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
                 process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
                 process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
@@ -24,19 +25,22 @@ module.exports = function(app)
                 process.env.OPENSHIFT_APP_NAME;
         }
 
+        // connection string for remote mLab instance of MongoDB
         connectionString = 'mongodb://mayank:mayank@ds033076.mlab.com:33076/webdev-mayank';
     }
 
+    // adding mongoose for manipulating Mongo DB
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
-    var TestSchema = mongoose.Schema({
-        message: String
-    });
+    // schema for database
+    var TestSchema = mongoose.Schema({ message: String });
 
     var TestModel = mongoose.model("TestModel", TestSchema);
 
-    function findAllMessages(req, res) {
+    // returns all the messages from DB
+    function findAllMessages(req, res)
+    {
         TestModel
             .find()
             .then(
@@ -49,7 +53,9 @@ module.exports = function(app)
             );
     }
 
-    function createMessage(req, res) {
+    // writes new messages to DB
+    function createMessage(req, res)
+    {
         TestModel
             .create(req.body)
             .then(
@@ -62,7 +68,9 @@ module.exports = function(app)
             );
     }
 
-    function deleteMessage(req, res) {
+    // deletes an existing message from DB
+    function deleteMessage(req, res)
+    {
         TestModel
             .remove({_id: req.params.id})
             .then(
